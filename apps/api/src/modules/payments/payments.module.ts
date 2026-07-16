7dc5ba58c20acb84108fcb3033a6e81589596167
martin-kambo <106653354+martin-kambo@users.noreply.github.com>
2026-07-16 09:38:10 +0300

    remove all duplicates

--- apps/api/src/modules/payments/payments.module.ts
+++ apps/api/src/modules/payments/payments.module.ts
@@ -10,19 +10,3 @@ import { PaymentsInfrastructureModule } from '../../infrastructure/payments/paym
   exports:     [PaymentsService],
 })
 export class PaymentsModule {}
-EOF
-cat "$REPO/apps/api/src/modules/payments/payments.module.ts"
-Output
-
-import { Module } from '@nestjs/common'
-import { PaymentsController }           from './payments.controller'
-import { PaymentsService }              from './payments.service'
-import { PaymentsInfrastructureModule } from '../../infrastructure/payments/payments-infrastructure.module'
-
-@Module({
-  imports:     [PaymentsInfrastructureModule],
-  controllers: [PaymentsController],
-  providers:   [PaymentsService],
-  exports:     [PaymentsService],
-})
-export class PaymentsModule {}
