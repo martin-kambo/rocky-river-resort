be29cf9501b1f6de1d33656f6f8f88990cc5027d
martin-kambo <106653354+martin-kambo@users.noreply.github.com>
2026-07-20 12:57:11 +0300

    use client

--- apps/web/src/components/sections/HeroSection.tsx
+++ apps/web/src/components/sections/HeroSection.tsx
@@ -1,6 +1,7 @@
-import Image from 'next/image'
 
 'use client'
+
+import Image from 'next/image'
 import { useTranslations } from 'next-intl'
 import { BookingWidget }   from '../booking/BookingWidget'
 
